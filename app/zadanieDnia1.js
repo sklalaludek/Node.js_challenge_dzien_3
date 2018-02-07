bbcbdd48d51e1b753a7fecad6e5866d554cf0613
sklalaludek <pattiegolis@gmail.com>
2018-02-07 21:29:21 +0100

    zadanie dnia 3 - 1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,14 @@
 //Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+const fileName = process.argv.slice(2);
+
+fs.readFile(`${fileName[0]}`, 'utf8', (error, data) => {
+    if (error === null) {
+        const hash = crypto.createHmac('sha256', data).digest('hex');
+        console.log(hash);
+    } else {
+        console.log('Błąd podczas odczytu pliku!', error);
+    }
+});
